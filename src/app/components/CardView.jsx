ceb8ee9c5cffc0658405b669cf76ea8d3879c5fc
ihsankarim <ihsanalkarim303@gmail.com>
2024-06-27 20:21:42 +0700

    update: add project spring

--- src/app/components/CardView.jsx
+++ src/app/components/CardView.jsx
@@ -5,16 +5,16 @@ function CardView({ title, description, imageSrc, link }) {
     <div className="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md m-2">
       {imageSrc && (
         <img
-          className="w-full h-56 object-cover object-center"
+          className="w-full h-56 p-2 rounded-2xl object-cover object-center"
           src={imageSrc}
           alt="Card"
         />
       )}
       <div className="p-4">
-        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+        <h2 className="text-xl text-neutral-950 font-semibold mb-2">{title}</h2>
         <p className="text-gray-600">{description}</p>
         {link && (
-            <Link href={link} className="mt-4 inline-block bg-blue-500 text-white py-2 px- rounded-md hover:bg-blue-600">
+            <Link href={link} className="mt-4 p-3 inline-block bg-blue-900 text-white py-2 px- rounded-md hover:bg-blue-600">
                 View Project
             </Link>
         )}
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,11 +7,11 @@ export default function Projects() {
       <Navbar />
       <div className="container mx-auto mt-24 px-24 py-4 gap-2 md:grid grid-cols-2  lg:grid-cols-4">
         <CardView
-          title="Github Android App"
-          description="Aplikasi untuk menampilkan user github"
-          imageSrc="/logo.png"
-          link="https://github.com/ihsankarim/GitHubUserApp"
-        />
+          title="PUAS"
+          description="Pusat Aspirasi adalah project community complaint system dengan teknologi Java Spring boot."
+          imageSrc="/img/projects/springboot.jpeg"
+          link="https://github.com/ihsankarim/puas-communitycomplaintsystem"
+          />
         <CardView
           title="Github Android App"
           description="Aplikasi untuk menampilkan user github"
